5a184e762af90d069a18b6b1e38a484d3f8c824b
KhalesArtem <dev.khales@gmail.com>
2024-03-26 14:28:19 +0200

    connect redis to project

--- src/config/app.php
+++ src/config/app.php
@@ -122,5 +122,8 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+    'aliases' => [
+        'Redis' => Illuminate\Support\Facades\Redis::class,
+    ],
 
 ];
